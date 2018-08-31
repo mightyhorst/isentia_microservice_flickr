9b2e95a7c8183f79c7fa343632b5046afa36977d
Nick Mitchell <nick@kitset.io>
2018-08-31 14:31:09 +1000

    Added Flickr service to wrap up the Flickr public API

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var ApiBuilder = require('claudia-api-builder'),
+	FlickrService = require('./services/FlickrService'), 
 	api = new ApiBuilder();
 
 api.get('/flickr', function(req, res, next) {
 
 	console.log('req', req ); 
+	var query = req.queryString,
+		body = req.bodypath,
+		params = req.pathParams;
 
-	return JSON.stringify(req);
+		FlickrService.getAll()
+			.then(flickrFeed => {
+				return flickrFeed
+			})
 })
 
 
---
+++ services/FlickrService.js
@@ -0,0 +1,22 @@
+var request = require('request-promise');
+
+var flickrApi = {
+	publicPhotosUrl: 'https://api.flickr.com/services/feeds/photos_public.gne?format=json'
+}
+
+var FlickrService = {
+	getAll: function() {
+
+		return new Promise(done, fail => {
+			request(flickrApi.publicPhotosUrl)
+				.then(flickrRes => {
+					done(flickrRes);
+				})
+				.catch(err => {
+					fail(err);
+				})
+		})
+		
+	}
+}
+module.exports = FlickrService;
